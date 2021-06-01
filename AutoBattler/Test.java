fb1880941c141a4933d9e9fffc96e4f61533b3fe
Amanda Chen <52475822+ash1024@users.noreply.github.com>
2021-05-31 23:46:34 -0400

    Add files via upload

--- AutoBattler/Test.java
+++ AutoBattler/Test.java
@@ -1,12 +1,13 @@
 package AutoBattler;
+import java.io.IOException;
 import java.util.*;
+import javax.swing.*;
 
 public class Test
 {
     private static Scanner in = new Scanner(System.in);
 
-    public static void recruit(Warband w)
-    {
+    public static void recruit(Warband w) throws IOException {
         for (int i = 0; i < 5; i++) {
             Fighter option1 = new Fighter((int) (Math.random() * 5), w);
             Fighter option2 = new Fighter((int) (Math.random() * 5), w);
@@ -26,17 +27,27 @@ public class Test
 
 
 
-    public static void main(String[] args) throws InterruptedException
-    {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        /*
         Warband warband1 = new Warband("player 1");
         recruit(warband1);
         System.out.println(warband1);
-        /*Warband warband2 = new Warband();
+        Warband warband2 = new Warband();
         System.out.println(warband2);
         Thread.sleep(1000);
         Fighter.attack(warband1.getFighter(0), warband2.getFighter((int) (warband2.getLength()*Math.random())));
-
          */
+
+        JFrame frame = new JFrame();
+
+        JLabel label = new JLabel();
+        ImageChanger imageChanger = new ImageChanger();
+        ImageIcon icon = new ImageIcon(imageChanger.invertColours("AutoBattlerImages/fighterImage00.png"));
+        label.setIcon(icon);
+        frame.add(label);
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
 
     }
 
